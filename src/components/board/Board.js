c0fa05d7a39b0496c442e68fa6b60bd3fba61a43
alwinpamintuan <jrpamintuan1@up.edu.ph>
2021-07-01 19:57:17 +0800

    feat: tictactoe ui

---
+++ src/components/board/Board.js
@@ -0,0 +1,23 @@
+import React, {useContext} from 'react'
+import {GameContext} from '../context/GameContext'
+import Cell from '../cell/Cell'
+import './Board.css'
+
+function Board(){
+    const {board} = useContext(GameContext)
+
+    return(
+        <div className="board">
+            {
+                board.map((row, x) => {
+                    return row.map((cell, y) => {
+                        return <Cell key={3*x + y} x={x} y={y}>{cell}</Cell>
+                    })
+                })
+            }
+        </div>
+
+    )
+}
+
+export default Board
